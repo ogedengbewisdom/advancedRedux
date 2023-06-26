eeba4c37df1fbb89eb75d473dc49d18f71247f1f
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-26 16:28:34 +0100

    configure store with configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
+import uiSlice from "./uiSlice";
 
 const store = configureStore({
     reducer: {
-        cart: cartSlice.reducer
+        cart: cartSlice.reducer,
+        toggle: uiSlice.reducer
     }
 })
 
