d2987f3b02c9d04b905fa07108cc5ea58739a01d
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-26 16:17:30 +0100

    wrapping list of product with card component

---
+++ src/components/Shop/ProductItem.js
@@ -0,0 +1,36 @@
+import { useDispatch } from 'react-redux';
+import Card from '../UI/Card';
+import classes from './ProductItem.module.css';
+import { cartAction } from '../../store/cartSlice';
+
+const ProductItem = (props) => {
+  const { title, price, description, id } = props;
+
+  const dispatch = useDispatch()
+
+  const addToCart = () => {
+    dispatch(cartAction.addItem({
+      id,
+      title, 
+      price, 
+      description
+    }))
+  }
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCart}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
