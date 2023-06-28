e02b01a062b286c1055cf659efea135149b16504
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-29 00:48:11 +0100

    making use of useEffect as side effect with redux and realtime database

--- src/App.js
+++ src/App.js
@@ -2,27 +2,76 @@ import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { Fragment } from 'react';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/uiSlice';
+import { useRef } from 'react';
 
 function App() {
   const show = useSelector(state => state.toggle.showCart)
   const cart = useSelector(state => state.cart)
-
+  const notification = useSelector(state => state.toggle.notification)
+  const dispatch = useDispatch()
+  
+ const isInitial = useRef(true)
   useEffect( () => {
-    fetch("https://redux-b81f4-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-      headers: {
-        "Content-Type": "application/json"
+    
+
+   const sendCartData = async () => {
+
+      dispatch(uiActions.showNotification({
+        title: "Sending...",
+        message: "Sending Request",
+        status: "pending"
+      }))
+
+        const sendRequest = async () => {
+          const response = await fetch("https://redux-b81f4-default-rtdb.firebaseio.com/cart.json", {
+            method: "PUT",
+            body: JSON.stringify(cart),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+          if (!response.ok) {
+            throw new Error ("Something went wrong try again later")
+          }
       }
-    })
-  }, [cart])
+
+      try {
+        await sendRequest()
+        dispatch(uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Successfully send Request"
+        }))
+      } catch(error) {
+        dispatch(uiActions.showNotification({
+          status: "error",
+          title: "Failed",
+          message: error.message
+        }))
+      }
+  
+    }
+    if ( isInitial.current ) {
+      isInitial.current = false;
+      return;
+    } else {
+      sendCartData() 
+    }
+    
+  }, [cart, dispatch])
+
   return (
-    <Layout>
-      {show && <Cart />}
-      <Products />
-    </Layout>
+      <Fragment>
+        {notification && <Notification title={notification.title} message={notification.message} status={notification.status} />}
+        <Layout>
+          {show && <Cart />}
+          <Products />
+        </Layout>
+      </Fragment>
   );
 }
 
