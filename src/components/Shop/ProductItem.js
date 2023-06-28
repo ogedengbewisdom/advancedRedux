7d1c65ccf426fd9edaa376571ecf76fca07c7c07
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-28 15:48:35 +0100

    use Custom hook to add item to cart

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -2,19 +2,21 @@ import { useDispatch } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartAction } from '../../store/cartSlice';
+import useAddToCart from '../../hooks/useAddToCart';
 
 const ProductItem = (props) => {
   const { title, price, description, id } = props;
+  const {items, totalQuantity} = useAddToCart({ title, price, id })
 
   const dispatch = useDispatch()
 
   const addToCart = () => {
-    dispatch(cartAction.addItem({
-      id,
-      title, 
-      price, 
-      description
-    }))
+    dispatch(cartAction.replace({items, totalQuantity}))
+    // dispatch(cartAction.addItem({
+    //   id,
+    //   title, 
+    //   price,
+    // }))
   }
 
   return (
