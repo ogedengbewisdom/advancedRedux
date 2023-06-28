8508f33576d6fd48841555dc9143a3c2a4a72b61
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-06-28 17:21:47 +0100

    root app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -6,6 +7,17 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const show = useSelector(state => state.toggle.showCart)
+  const cart = useSelector(state => state.cart)
+
+  useEffect( () => {
+    fetch("https://redux-b81f4-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }, [cart])
   return (
     <Layout>
       {show && <Cart />}
